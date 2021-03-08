6e3789b6f5d9dc7ac8af6535f728fbb6771f42ca
fviel <fernandovie82@gmail.com>
2021-03-07 22:28:43 -0300

    fazendo chamada http em uma api json de testes decodificando json string em um objeto tudo isso de forma assíncrona

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,69 +6,20 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
-  int counter = 0;
-
-  /**
-   * simula um request de tempo variável para usar código assíncrono, pois um ws pode demorar x tempo...
-   */
-  void getData() async{
-    //chama assincronamente, mas o 'await' define que o reto do método tem que esperar esse aqui primeiro
-    //observe que a função dentro do Future vai retornar uma string pra var nome
-    String nome = await Future.delayed(Duration(seconds:3), (){
-       print('_ChooseLocationState.getDate ------ simula o request com delay 1 -------');
-       return 'fernando';
-    });
-
-    //simula o request com delay obtendo a bio
-    Future.delayed(Duration(seconds:3), (){
-      print('_ChooseLocationState.getDate ------ simula o request com delay 2 - aguarda o 1 -------');
-    });
-
-    //simula o request com delay obtendo a bio
-    Future.delayed(Duration(seconds:3), (){
-      print('_ChooseLocationState.getDate ------ simula o request com delay 3 - aguarda o 1 -------');
-    });
-
-    print('mensagem após a função assíncrona ser chamada... $nome');
-  }
-  /**
-   *  // initState roda uma única vez, ao instanciar o widget
-   */
-  @override
-  void initState() {
-
-    super.initState();
-    print('_ChooseLocationState.initState executou---------');
-    getData();
-    print('método após o getData() async');
-  }
-
-
   /**
    * build é executada sempre que realizado o setState();
    */
   @override
   Widget build(BuildContext context) {
-
     print('_ChooseLocationState.build executou---------');
 
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose a location'),
-        centerTitle: true,
-        elevation:0
-      ),
-      //não precisa do safeArea, pois vou usar um appBar
-      body: RaisedButton(
-        onPressed: (){
-          setState(() {
-            counter++;
-          });
-        },
-        child: Text('Contador: $counter'),
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose a location'),
+          centerTitle: true,
+          elevation: 0
       ),
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,6 +8,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  /**
+   * simula um request de tempo variável para usar código assíncrono, pois um ws pode demorar x tempo...
+   */
+  void getData() async{
+    //obtém a resposta de uma chamada neste endereço
+    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    print('Resposta da chamada assíncrona http: $response.body');
+    print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data['title']);
+  }
+  /**
+   *  // initState roda uma única vez, ao instanciar o widget
+   */
+  @override
+  void initState() {
+
+    super.initState();
+    print('_ChooseLocationState.initState executou---------');
+    getData();
+    print('método após o getData() async');
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
