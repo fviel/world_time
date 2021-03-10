1f1d47607608149f8b8e01b4c52c529c7acce8fb
fviel <fernandovie82@gmail.com>
2021-03-10 13:57:41 -0300

    adicionado o vetor de países com nomes e suas bandeiras

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +7,21 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  //https://www.countryflags.io/:country_code/:style/:size.png"
+  List<WorldTime> locations = [
+    WorldTime(url:'America/Sao_Paulo', location:'São Paulo', flag:'https://www.countryflags.io/br/flat/64.png'),
+    WorldTime(url:'Europe/London', location:'London', flag:'https://www.countryflags.io/gb/flat/64.png'),
+    WorldTime(url:'Europe/Berlin', location:'Berlin', flag:'https://www.countryflags.io/de/flat/64.png'),
+    WorldTime(url:'Africa/Cairo', location:'Cairo', flag:'https://www.countryflags.io/ed/flat/64.png'),
+    WorldTime(url:'Africa/Nairobi', location:'Nairobi', flag:'https://www.countryflags.io/ke/flat/64.png'),
+    WorldTime(url:'America/Chicago', location:'Chicago', flag:'https://www.countryflags.io/us/flat/64.png'),
+    WorldTime(url:'America/New_York', location:'New York', flag:'https://www.countryflags.io/us/flat/64.png'),
+    WorldTime(url:'Asia/Seoul', location:'Seoul', flag:'https://www.countryflags.io/kr/flat/64.png'),
+    WorldTime(url:'Asia/Jakarta', location:'Jakarta', flag:'https://www.countryflags.io/id/flat/64.png'),
+  ];
+
+
   /**
    * build é executada sempre que realizado o setState();
    */
